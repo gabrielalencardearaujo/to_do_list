3fcc92be88047c264e3a2d48bc47c36737815b16
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-10-26 23:24:57 -0300

    custom buttons iconFiel/iconFolder

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from '../assets/css/SideNav.module.css';
-import IconFileSVG from '../assets/img/IconFileSVG';
-import {ReactComponent as IconFolder} from '../assets/img/iconFolder.svg';
+import {ReactComponent as IconFolder } from '../assets/img/iconFolder.svg';
+import {ReactComponent as IconFile} from '../assets/img/iconFile.svg';
 
 function SideNav() {
   return (
     <section className={styles.sideBarContainer}>
       <div className={styles.title}>To do List</div>
       <div className={styles.iconsDiv}>
-        <IconFileSVG />
-        <IconFolder />
+        <IconFolder className={styles.icon}/>
+        <IconFile className={styles.icon} />
       </div>
+      
     </section>
   )
 }
