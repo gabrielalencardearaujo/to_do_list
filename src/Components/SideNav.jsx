2c397522f1c9d953419973396e1dbb8923342777
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-10-26 22:34:02 -0300

    add title and icons newfile/folder

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from '../assets/css/SideNav.module.css';
+import IconFileSVG from '../assets/img/IconFileSVG';
+import {ReactComponent as IconFolder} from '../assets/img/iconFolder.svg';
 
 function SideNav() {
   return (
     <section className={styles.sideBarContainer}>
       <div className={styles.title}>To do List</div>
+      <div className={styles.iconsDiv}>
+        <IconFileSVG />
+        <IconFolder />
+      </div>
     </section>
   )
 }
