829d709b195897d8d5d0a832f29b501d1c24cab9
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-10-30 19:29:40 -0300

    add page home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import '../assets/css/App.css';
+import styles from '../assets/css/TaskContext.module.css';
 import SideNav from './SideNav'
 import TaskContext from './TaskContext'
 import useLocalStorage from '../Hooks/useLocalStorage';
 import TaskHome from './TaskHome';
-import styles from '../assets/css/TaskContext.module.css';
 
 function Home() {
-  const {data, setData} = useLocalStorage();
+  const { data, setData } = useLocalStorage();
   const [taskActive, setTaskActive] = React.useState({});
 
   return (
     <main>
-      <SideNav 
-      setData={setData} 
-      data={data} 
-      setTaskActive={setTaskActive} />
+      <SideNav
+        setData={setData}
+        data={data}
+        setTaskActive={setTaskActive} />
       <section className={styles.container}>
         {(taskActive.title === undefined) ? (
           <TaskHome />
         ) : (
-        <TaskContext taskActive ={taskActive} setData={setData} />
+          <TaskContext taskActive={taskActive} setData={setData} />
         )}
       </section>
     </main>
-    
+
   )
 }
 
-export default Home
+export default Home;
--- src/Components/TaskHome.jsx
+++ src/Components/TaskHome.jsx
@@ -3,8 +3,12 @@ import styles from '../assets/css/TaskHome.module.css';
 
 function TaskHome() {
   return (
-    <div className={styles.container}>
-      alguma coisa
+    <div className={styles.containerHome}>
+      <div>
+        <h1>To do List</h1>
+        <p>Create new Folder ( Ctrl + , )</p>
+        <p>Create new File ( Ctrl + . )</p>
+      </div>
     </div>
   )
 }
