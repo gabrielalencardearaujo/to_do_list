54ec521f9ebe7599639c0cf12d35ae8b0e41de29
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-12-05 20:49:34 -0300

    adicionado icon menu e sua funcionalidade

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,48 +1,69 @@
-import React from 'react';
-import styles from '../assets/css/SideNav.module.css';
-import {ReactComponent as IconFolder } from '../assets/img/iconFolder.svg';
-import {ReactComponent as IconFile} from '../assets/img/iconFile.svg';
-import Task from './Task';
-import useLocalStorage, { newTask } from '../Hooks/useLocalStorage';
+import React from "react";
+import styles from "../assets/css/SideNav.module.css";
+import { ReactComponent as IconFile } from "../assets/img/iconFile.svg";
+import { ReactComponent as IconMenu } from "../assets/img/iconMenu.svg";
+import Task from "./Task";
+import { newTask } from "../Hooks/useLocalStorage";
 
-function SideNav({data, setData, setTaskActive, taskActive}) {
+function SideNav({ data, setData, setTaskActive, taskActive }) {
+  const [menu, setMenu] = React.useState(false);
 
   // Criar um novo arquivo/task
   function handleClick() {
     const ID = newTask(data.length);
-    
-    if(data.length === 0) {
+
+    if (data.length === 0) {
       setData([ID]);
-      setTaskActive({...ID})
-    } else if(data[data.length - 1].title !== 'undefined' ) {
-      setData((value) => [...value, ID])
-      setTaskActive({...ID})
+      setTaskActive({ ...ID });
+    } else if (data[data.length - 1].title !== "undefined") {
+      setData((value) => [...value, ID]);
+      setTaskActive({ ...ID });
     }
   }
-  
+
+  function toggleMenu({ target }) {
+    setMenu(!menu);
+  }
+
   return (
-    <section className={styles.sideBarContainer}>
-      <div className={styles.title}>To do List</div>
+    <section
+      className={
+        menu
+          ? `${styles.sideBarContainer}`
+          : `${styles.sideBarContainer} ${styles.menuClosed}`
+      }
+    >
+      <div className={menu ? styles.title : `${styles.title} ${styles.center}`}>
+        <IconMenu onClick={toggleMenu} className={styles.iconMenu} />
+        {menu ? <p>To do List</p> : ""}
+      </div>
+
       <div className={styles.iconsDiv}>
         <IconFile onClick={handleClick} className={styles.icon} />
       </div>
 
-      <div className={styles.taskContainer}>
-        {data && data.map((task, index) => (
-          <Task 
-          key={index}
-          title={task.title}
-          id={task.id}
-          setData={setData}
-          data={data}
-          setTaskActive={setTaskActive}
-          taskActive={taskActive}
-           />
-        ))}
+      <div
+        className={
+          menu
+            ? `${styles.taskContainer}`
+            : `${styles.taskContainer} ${styles.notVisible}`
+        }
+      >
+        {data &&
+          data.map((task, index) => (
+            <Task
+              key={index}
+              title={task.title}
+              id={task.id}
+              setData={setData}
+              data={data}
+              setTaskActive={setTaskActive}
+              taskActive={taskActive}
+            />
+          ))}
       </div>
-      
     </section>
-  )
+  );
 }
 
-export default SideNav
+export default SideNav;
--- src/Components/Taks2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Taks2() {
-  return (
-    <div>Taks</div>
-  )
-}
-
-export default Taks2
